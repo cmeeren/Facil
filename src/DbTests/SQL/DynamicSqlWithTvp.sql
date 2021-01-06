f3db5bca10181ed4f6e2126709ea715ebad8e92e
Christer van der Meeren <cmeeren@protonmail.com>
2021-01-06 11:04:35 +0100

    Add test for dynamic SQL with TVP

---
+++ src/DbTests/SQL/DynamicSqlWithTvp.sql
@@ -0,0 +1,7 @@
+﻿DECLARE @sql NVARCHAR(MAX) =
+  'SELECT * FROM dbo.Table1 WHERE TableCol2 IN (SELECT * FROM @tvp)'
+
+DECLARE @paramList NVARCHAR(MAX) =
+  '@tvp dbo.SingleColNonNull READONLY'
+
+EXEC sp_executesql @sql, @paramList, @tvp
